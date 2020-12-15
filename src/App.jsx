00abe6ba48912ed2412a12084cd0092c734fe9d0
RylePC <urciaryle@gmail.com>
2020-12-15 12:27:57 -0500

    refactor: Deleted console logs and changed routes
    Sign In Page route changed from '/' to '/SignIn'

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
+// Component(s)
+import { NavBar } from './Components/NavBar';
 // Pages
 import { SignUpPage } from './Pages/SignUpPage';
 import { SignInPage } from './Pages/SignInPage';
@@ -9,18 +11,20 @@ import { PrivateRoute } from './Components/PrivateRoute';
 // Global Style
 import { GlobalStyle } from './Styled-Components/GlobalStyles';
 
-function App() {
-
-  console.log(localStorage.getItem('loggedIn'));
+const App = () => {
 
   return (
     <div className="App">
       <GlobalStyle />
       <AnimatePresence>
         <Switch>
+          {/* Default Route will be /home */}
+          <Route exact path='/'>
+            <Redirect to="/home" />
+          </ Route>
           <PrivateRoute exact path='/home' component={HomePage} />
           <Route exact path='/SignUp' component={SignUpPage} />
-          <Route exact path='/' component={SignInPage} />
+          <Route exact path="/SignIn" component={SignInPage} />
         </Switch>
       </AnimatePresence>
     </div>
