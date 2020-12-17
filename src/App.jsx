84de2ccc218b17f2a3a36842870414a0e231678c
RylePC <urciaryle@gmail.com>
2020-12-17 13:23:26 -0500

    style: Made App container into a flexbox
    App container is now displaying flex so that nav bar is on the left

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
-// Component(s)
-import { NavBar } from './Components/NavBar';
 // Pages
 import { SignUpPage } from './Pages/SignUpPage';
 import { SignInPage } from './Pages/SignInPage';
 import { HomePage } from './Pages/HomePage';
+import { ProfilePage } from './Pages/ProfilePage';
 import { PrivateRoute } from './Components/PrivateRoute';
+// Components
+import NavBar from './Components/Nav/NavBar';
 // Global Style
 import { GlobalStyle } from './Styled-Components/GlobalStyles';
 
 const App = () => {
 
   return (
-    <div className="App">
+    <StyledApp>
       <GlobalStyle />
+      <NavBar />
       <AnimatePresence>
         <Switch>
           {/* Default Route will be /home */}
@@ -23,12 +26,20 @@ const App = () => {
             <Redirect to="/home" />
           </ Route>
           <PrivateRoute exact path='/home' component={HomePage} />
+          <PrivateRoute exact path='/profile' component={ProfilePage} />
           <Route exact path='/SignUp' component={SignUpPage} />
           <Route exact path="/SignIn" component={SignInPage} />
         </Switch>
       </AnimatePresence>
-    </div>
+    </StyledApp>
   );
 }
 
 export default App;
+
+
+const StyledApp = styled.div`
+width:100%;
+display: flex;
+flex-direction: row;
+`
